082e2e8e689063a138a400e4b7cf453d73998e39
Quinten De Swaef <quintendeswaef@gmail.com>
2017-03-02 08:16:42 +0100

    fixing tests and placeholders

--- src/test/java/eu/deswaef/cinderpup/roboinsta/instagram/io/Instagram4jTest.java
+++ src/test/java/eu/deswaef/cinderpup/roboinsta/instagram/io/Instagram4jTest.java
@@ -1,24 +1,34 @@
 package eu.deswaef.cinderpup.roboinsta.instagram.io;
 
+import eu.deswaef.cinderpup.roboinsta.RoboInstaApplication;
 import eu.deswaef.cinderpup.roboinsta.instagram.io.tags.InstagramSearchTagsRequest;
 import eu.deswaef.cinderpup.roboinsta.instagram.io.tags.InstagramSearchTagsResultTag;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
+@SpringBootTest(classes = RoboInstaApplication.class)
 @RunWith(SpringRunner.class)
 @ActiveProfiles("integrationtest")
 public class Instagram4jTest {
 
     private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private Environment environment;
 
     @Value("${roboinsta.credentials.username}")
     private String userName;
